5091ce70af485fb941eb88ddc1747207b55a06aa
joelhng <joelhng@gmail.com>
2021-09-21 19:13:07 -0300

    Inicio del proyecto
    Proyecto hecho en C. Para la facultad.
    Incluye un main más las librerias utilizadas para la calculadora.

---
+++ TP_1/src/TP_1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TP_1.cpp
+// Author      : joelhng
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
